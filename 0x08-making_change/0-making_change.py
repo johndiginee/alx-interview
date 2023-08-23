573615f3cae56a835ccf8d627b6457de9954235e
John Diginee <Johndbizz@gmail.com>
2023-08-23 14:18:17 +0100

    Chore: Fix bug

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 """Function to determine the fewest number of coins needed
-   to meet a given amount total"""
+   to meet a given amount total.
+"""
 
 
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while (total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
