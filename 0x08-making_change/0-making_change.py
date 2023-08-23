26c03c0b8a51445a15d03bc995e39629bf8293cc
John Diginee <Johndbizz@gmail.com>
2023-08-23 14:13:11 +0100

    Chore: Make code PEP 8 compliant

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,12 +6,9 @@
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    
-    dp = [float('inf')] * (total + 1)  # Initialize DP array
-    dp[0] = 0  # Base case: 0 coins needed for 0
-    
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
     for coin in coins:
         for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)  # Update minimum coins
-    
-    return dp[total] if dp[total] != float('inf') else -1  # Return result
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    return dp[total] if dp[total] != float('inf') else -1
